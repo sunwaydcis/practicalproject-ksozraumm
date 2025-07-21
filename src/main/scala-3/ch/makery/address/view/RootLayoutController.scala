3d4f455633fddaefba32df5ad6eb7ad35de41d84
ksozraumm <jaimelynnwongsy@gmail.com>
2025-07-21 17:42:04 +0800

    Update

---
+++ src/main/scala-3/ch/makery/address/view/RootLayoutController.scala
@@ -0,0 +1,22 @@
+package ch.makery.address.view
+
+import ch.makery.address.MainApp
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+
+@FXML
+class RootLayoutController():
+  @FXML
+  def handleClose(action: ActionEvent): Unit = {
+    MainApp.stage.close()
+    //System.exit(0) for force exit
+  }
+
+  @FXML
+  def handleDelete(action: ActionEvent): Unit = {
+    //done to access the personoverviewcontroller because the function is already made
+    MainApp.overviewControl.map(x => x.handleDeletePerson(action))
+  }
+
+
+
